5fe6e224b6764aeb8efb82ebf5ee096dc823a3b9
Yu Pan <70486009+yu-pan-cpu@users.noreply.github.com>
2022-04-08 15:59:29 -0400

    set up navbar

--- components/navbar/NavBarBase.tsx
+++
@@ -1,3 +0,0 @@
-// this is the cool sidebar that will allow the user to automatically click to a specific section of the site
-
-export {};
---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import { Text } from "@chakra-ui/react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Text>Fancy routing ~~~~</Text>
+    </div>
+  );
+};
+
+export default Navbar;
