b178866e3dacc346715b83edd0f3b1fd4fd1f3f8
Yu Pan <70486009+yu-pan-cpu@users.noreply.github.com>
2022-05-15 15:41:05 -0400

    add basic Main template

--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -1,9 +1,19 @@
-import { Text } from "@chakra-ui/react";
+import { Button, Center, VStack, Text } from "@chakra-ui/react";
+import styles from "./Main.module.scss";
 
 const Main = () => {
   return (
-    <div>
-      <Text>Where the fancy start screen should be</Text>
+    <div className={styles.title_box}>
+      <Center>
+        <Text className={styles.title}>HackGT#</Text>
+      </Center>
+      <Center className={styles.description}>
+        <VStack spacing="0.6rem" padding="1rem">
+          <Text>Some witty catchphrase or something</Text>
+          <Text>Coming soon to colleges near you</Text>
+          <Button className={styles.register_button}>Register now</Button>
+        </VStack>
+      </Center>
     </div>
   );
 };
