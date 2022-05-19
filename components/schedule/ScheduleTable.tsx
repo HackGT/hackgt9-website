88595f483b72b1be7e75e9903f1606d6499a8ff9
Yu Pan <70486009+yu-pan-cpu@users.noreply.github.com>
2022-05-19 01:19:52 -0400

    set up schedule data

--- components/schedule/ScheduleTable.tsx
+++ components/schedule/ScheduleTable.tsx
@@ -1,8 +1,18 @@
 import { Text } from "@chakra-ui/react";
+import { useState } from "react";
 
 import styles from "./Schedule.module.scss";
 
+import events from "./ScheduleEvents.json";
+
 const ScheduleTable = () => {
+  const [eventDays] = useState([4, 5, 6]);
+
+  // as a note the json days go from 0 - 6, so 0 means Monday, 6, means Sunday
+  const getSpecificDayEvents = (day: number) => {
+    return 0;
+  };
+
   return (
     <div className={styles.schedule}>
       <Text>Where the schedule should be</Text>
