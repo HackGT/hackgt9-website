9622d8c9d876dee615f2549edb2f4867ebaf718a
Yu Pan <70486009+yu-pan-cpu@users.noreply.github.com>
2022-06-27 20:08:37 -0400

    convert footer to chakra ui, add footer styling

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,26 +1,33 @@
+import { HStack, VStack, Text, Link, Image } from "@chakra-ui/react";
+import styles from "./Footer.module.scss";
+
 const Footer = () => {
-    return (
-      <div>
-          <footer>
-              <p>With 💜 from the HexLabs team</p>
-              <div className="icon-container">
-              <a href="https://www.instagram.com/thehexlabs/"
-                  ><img src="/icon/insta.svg"
-              /></a>
-              <a href="https://www.facebook.com/TheHexLabs"
-                  ><img src="/icon/fb.svg"
-              /></a>
-              <a href="https://twitter.com/TheHexLabs"
-                  ><img src="/icon/twitter.svg"
-              /></a>
-              <a href="https://github.com/HackGT"
-                  ><img src="/icon/github.svg"
-              /></a>
-              <a href="https://hexlabs.org/"><img src="/icon/web.svg" /></a>
-              </div>
-          </footer>
-      </div>
-    );
-  };
-  
-  export default Footer;
+  return (
+    <div className={styles.footer_container}>
+      <footer>
+        <VStack className={styles.footer_content}>
+          <HStack>
+            <Link href="https://www.instagram.com/thehexlabs/">
+              <Image src="/icon/insta.svg" alt="instagram" />
+            </Link>
+            <Link href="https://www.facebook.com/TheHexLabs">
+              <Image src="/icon/fb.svg" alt="facebook" />
+            </Link>
+            <Link href="https://twitter.com/TheHexLabs">
+              <Image src="/icon/twitter.svg" alt="twitter" />
+            </Link>
+            <Link href="https://github.com/HackGT">
+              <Image src="/icon/github.svg" alt="github" />
+            </Link>
+            <Link href="https://hexlabs.org/">
+              <Image src="/icon/web.svg" alt="hexlabs" />
+            </Link>
+          </HStack>
+          <Text>With 💜 from the HexLabs team</Text>
+        </VStack>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
