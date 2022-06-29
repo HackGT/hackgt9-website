b1af60f13650199664c0ea3f3a543b1d121d1ec6
naomesk <68748456+naomesk@users.noreply.github.com>
2022-06-29 15:33:39 +0300

    font and tetris changes

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -8,11 +8,11 @@ const About = () => {
       <Box className={styles.mainBox}>
           <Text className={styles.title}>About</Text>
           <Text className={styles.bodyText}>Discover your craft this fall at HackGT 8 from October 22nd - 24th!
-            We're back with the eighth iteration of our flagship event where we bring you to the HackGT 
+            {`We're`} back with the eighth iteration of our flagship event where we bring you to the HackGT 
             marketplace to support you as you hone your hacking skills and explore a weekend filled with 
             community and compassion.</Text>
             {/* There's supposed to be a tetris img sort of thing here. */}
-            <HStack>
+            {/* <HStack>
             <Grid className={styles.tetris1} gap={0} templateAreas={`"a b c" "d e f"`}>
               <GridItem area={"d"}>
                 <Image src="/img/about/tetris1.png" alt="tetris"></Image>
@@ -42,7 +42,33 @@ const About = () => {
                 <Image src="/img/about/tetris2.png" alt="tetris"></Image>
               </GridItem>
             </Grid>
-            </HStack>
+            </HStack> */}
+            <Grid className={styles.tetris} templateAreas={`"a b c d e" "f g h i j" "k l m n o"`}>
+              <GridItem  area={"k"}>
+                <Image src="/img/about/tetris1.png" alt="tetris"></Image>
+              </GridItem>
+              <GridItem  area={"l"}>
+                <Image src="/img/about/tetris1.png" alt="tetris"></Image>
+              </GridItem>
+              <GridItem  area={"m"}>
+                <Image src="/img/about/tetris1.png" alt="tetris"></Image>
+              </GridItem>
+              <GridItem   area={"g"}>
+                <Image src="/img/about/tetris1.png" alt="tetris"></Image>
+              </GridItem>
+              <GridItem  area={"e"}>
+                <Image src="/img/about/tetris2.png" alt="tetris"></Image>
+              </GridItem>
+              <GridItem  area={"n"}>
+                <Image src="/img/about/tetris2.png" alt="tetris"></Image>
+              </GridItem>
+              <GridItem area={"o"}>
+                <Image src="/img/about/tetris2.png" alt="tetris"></Image>
+              </GridItem>
+              <GridItem area={"j"}>
+                <Image src="/img/about/tetris2.png" alt="tetris"></Image>
+              </GridItem>
+            </Grid>
       </Box>
       <Image src="/img/about/radgrad4.png" alt="radius gradient" className={styles.radgrad3} ></Image>
       <Image src="/img/about/radgrad3.png" alt="radius gradient" className={styles.radgrad4} ></Image>
