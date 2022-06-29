7169e6a28032d8b4c711f60c6941987662b70a54
Yu Pan <70486009+yu-pan-cpu@users.noreply.github.com>
2022-06-29 19:22:24 -0400

    fix navbar error

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,46 +1,67 @@
-import { Text, Box, Image, Grid, GridItem, HStack} from "@chakra-ui/react";
-import styles from "./About.module.scss"
+import { Text, Box, Image, Grid, GridItem, HStack, Center } from "@chakra-ui/react";
+import styles from "./About.module.scss";
 const About = () => {
   return (
     <div className={styles.container}>
-      <Image src="/img/about/radgrad1.png" alt="radius gradient" className={styles.radgrad1} ></Image>
-      <Image src="/img/about/radgrad2.png" alt="radius gradient" className={styles.radgrad2} ></Image>
-      <Box className={styles.mainBox}>
+      <Image
+        src="/img/about/radgrad1.png"
+        alt="radius gradient"
+        className={styles.radgrad1}
+      ></Image>
+      <Image
+        src="/img/about/radgrad2.png"
+        alt="radius gradient"
+        className={styles.radgrad2}
+      ></Image>
+      <Center>
+        <Box className={styles.mainBox}>
           <Text className={styles.title}>About</Text>
-          <Text className={styles.bodyText}>Discover your craft this fall at HackGT 8 from October 22nd - 24th!
-            {`We're`} back with the eighth iteration of our flagship event where we bring you to the HackGT 
-            marketplace to support you as you hone your hacking skills and explore a weekend filled with 
-            community and compassion.</Text>
-            <Grid className={styles.tetris} templateAreas={`"a b c d e" "f g h i j" "k l m n o"`}>
-              <GridItem  area={"k"}>
-                <Image src="/img/about/tetris1.png" alt="tetris"></Image>
-              </GridItem>
-              <GridItem  area={"l"}>
-                <Image src="/img/about/tetris1.png" alt="tetris"></Image>
-              </GridItem>
-              <GridItem  area={"m"}>
-                <Image src="/img/about/tetris1.png" alt="tetris"></Image>
-              </GridItem>
-              <GridItem   area={"g"}>
-                <Image src="/img/about/tetris1.png" alt="tetris"></Image>
-              </GridItem>
-              <GridItem  area={"e"}>
-                <Image src="/img/about/tetris2.png" alt="tetris"></Image>
-              </GridItem>
-              <GridItem  area={"n"}>
-                <Image src="/img/about/tetris2.png" alt="tetris"></Image>
-              </GridItem>
-              <GridItem area={"o"}>
-                <Image src="/img/about/tetris2.png" alt="tetris"></Image>
-              </GridItem>
-              <GridItem area={"j"}>
-                <Image src="/img/about/tetris2.png" alt="tetris"></Image>
-              </GridItem>
-            </Grid>
-      </Box>
-      <Image src="/img/about/radgrad4.png" alt="radius gradient" className={styles.radgrad3} ></Image>
-      <Image src="/img/about/radgrad3.png" alt="radius gradient" className={styles.radgrad4} ></Image>
-      
+
+          <Text className={styles.bodyText}>
+            Discover your craft this fall at HackGT 8 from October 22nd - 24th!
+            {`We're`} back with the eighth iteration of our flagship event where we bring you to the
+            HackGT marketplace to support you as you hone your hacking skills and explore a weekend
+            filled with community and compassion.
+          </Text>
+
+          <Grid className={styles.tetris} templateAreas={`"a b c d e" "f g h i j" "k l m n o"`}>
+            <GridItem area={"k"}>
+              <Image src="/img/about/tetris1.png" alt="tetris"></Image>
+            </GridItem>
+            <GridItem area={"l"}>
+              <Image src="/img/about/tetris1.png" alt="tetris"></Image>
+            </GridItem>
+            <GridItem area={"m"}>
+              <Image src="/img/about/tetris1.png" alt="tetris"></Image>
+            </GridItem>
+            <GridItem area={"g"}>
+              <Image src="/img/about/tetris1.png" alt="tetris"></Image>
+            </GridItem>
+            <GridItem area={"e"}>
+              <Image src="/img/about/tetris2.png" alt="tetris"></Image>
+            </GridItem>
+            <GridItem area={"n"}>
+              <Image src="/img/about/tetris2.png" alt="tetris"></Image>
+            </GridItem>
+            <GridItem area={"o"}>
+              <Image src="/img/about/tetris2.png" alt="tetris"></Image>
+            </GridItem>
+            <GridItem area={"j"}>
+              <Image src="/img/about/tetris2.png" alt="tetris"></Image>
+            </GridItem>
+          </Grid>
+        </Box>
+      </Center>
+      <Image
+        src="/img/about/radgrad4.png"
+        alt="radius gradient"
+        className={styles.radgrad3}
+      ></Image>
+      <Image
+        src="/img/about/radgrad3.png"
+        alt="radius gradient"
+        className={styles.radgrad4}
+      ></Image>
     </div>
   );
 };
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import NavbarComponent from "./NavbarComponent";
 
 import styles from "./Navbar.module.scss";
-import { HStack, Box, Menu, MenuButton, MenuItem, IconButton, MenuList} from "@chakra-ui/react";
+import { HStack, Box, Menu, MenuButton, MenuItem, IconButton, MenuList } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 
 interface Anchor {
@@ -13,37 +13,38 @@ interface NavProps {
   anchors: Anchor[];
 }
 
-window.onscroll = function() {myFunction()};
-
 const Navbar = (np: NavProps) => {
   return (
     <div>
       <Box className={styles.navbg}>.</Box>
-      <Box className={styles.progressBar} id='myBar'>.</Box>
-    <HStack className={styles.container} spacing={"10vw"}>
-      {np.anchors.map((anchor) => (
-        <NavbarComponent key={null} name={anchor.name} anchorLink={anchor.id} />
-      ))}
-    </HStack>
+      <Box className={styles.progressBar} id="myBar">
+        .
+      </Box>
+      <HStack className={styles.container} spacing={"10vw"}>
+        {np.anchors.map((anchor) => (
+          <NavbarComponent key={null} name={anchor.name} anchorLink={anchor.id} />
+        ))}
+      </HStack>
 
-    <div className={styles.menu}>
-      <Menu id={"menu"} >
-        <MenuButton className={styles.menuButton}
-          as={IconButton}
-          aria-label='Options'
-          icon={<HamburgerIcon />}
-          variant='outline'
-        />
-        <MenuList className={styles.menuList}>
-          {np.anchors.map((anchor) => (
-            <MenuItem className={styles.menuItem}>
+      <div className={styles.menu}>
+        <Menu id={"menu"}>
+          <MenuButton
+            className={styles.menuButton}
+            as={IconButton}
+            aria-label="Options"
+            icon={<HamburgerIcon />}
+            variant="outline"
+          />
+          <MenuList className={styles.menuList}>
+            {np.anchors.map((anchor) => (
+              <MenuItem key={null} className={styles.menuItem}>
                 <NavbarComponent key={null} name={anchor.name} anchorLink={anchor.id} />
-            </MenuItem>
-          ))}
-        </MenuList>
-      </Menu>
+              </MenuItem>
+            ))}
+          </MenuList>
+        </Menu>
+      </div>
     </div>
-  </div>
   );
 };
 
@@ -51,7 +52,7 @@ function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = 90 - (winScroll / height) * 90;
-  document.getElementById('myBar').style.width = 90 - scrolled + "%";
+  document.getElementById("myBar").style.width = 90 - scrolled + "%";
 }
 
 export default Navbar;
