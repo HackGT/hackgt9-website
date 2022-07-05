4c12e1d7081d325ac2340780ac0bf803ccf8347d
Yu Pan <70486009+yu-pan-cpu@users.noreply.github.com>
2022-07-04 21:56:46 -0400

    add tracks sans glowing assets

--- src/components/tracks_and_challenges/BaseChallengeTrack.tsx
+++ src/components/tracks_and_challenges/BaseChallengeTrack.tsx
@@ -1,37 +1,63 @@
-import { Text, HStack, Center, VStack, Wrap } from "@chakra-ui/react";
-import BaseComponent from "./ChallengeTrackComponent";
+import { Text, HStack, Center, VStack, Wrap, Image, Box } from "@chakra-ui/react";
+import BaseComponent from "./BaseComponent";
 
 import styles from "./TracksChallenges.module.scss";
 
 import tracks from "./tracks.json";
-import challenges from "./challenges.json";
 
 const ChallengeTrackBase = () => {
   return (
     <Center className={styles.container}>
       <VStack>
-        <Text className={styles.header}>Tracks and Challenges</Text>
         <Text className={styles.header}>Tracks</Text>
-        <Wrap spacing={"10vw"} justify={"center"}>
-          {tracks.tracks.map((track) => (
-            <BaseComponent
-              key={track.name}
-              name={track.name}
-              description={track.description}
-              prize={null}
+        <Text className={styles.subtext}>
+          Are you an experienced or a beginner player in the hackathon game? We have two modes for
+          you to select from. Click each track to learn more.
+        </Text>
+
+        <Wrap justify={"center"} spacing={{ base: "0px", md: "7vw" }}>
+          <VStack>
+            <Image
+              alt="blue console"
+              src="/img/tracks/player_blue.png"
+              boxSize={{ base: "60vw", md: "30vw", lg: "25vw" }}
+              objectFit="contain"
             />
-          ))}
-        </Wrap>
-        <Text className={styles.header}>Challenges</Text>
-        <Wrap spacing={"10vw"} justify={"center"}>
-          {challenges.challenges.map((challenge) => (
-            <BaseComponent
-              key={challenge.name}
-              name={challenge.name}
-              description={challenge.description}
-              prize={challenge.prize}
+
+            <Box
+              className={styles.track_container}
+              width={{ base: "70vw", lg: "500px" }}
+              minHeight={{ base: "none", md: "380px", lg: "500px" }}
+            >
+              <BaseComponent
+                key={tracks.tracks[0].name}
+                name={tracks.tracks[0].name}
+                description={tracks.tracks[0].description}
+                prize={null}
+              />
+            </Box>
+          </VStack>
+          <VStack>
+            <Image
+              alt="pink console"
+              src="/img/tracks/player_pink.png"
+              boxSize={{ base: "60vw", md: "30vw", lg: "25vw" }}
+              objectFit="contain"
             />
-          ))}
+
+            <Box
+              className={styles.track_container}
+              width={{ base: "70vw", lg: "500px" }}
+              minHeight={{ base: "none", md: "470px", lg: "500px" }}
+            >
+              <BaseComponent
+                key={tracks.tracks[1].name}
+                name={tracks.tracks[1].name}
+                description={tracks.tracks[1].description}
+                prize={null}
+              />
+            </Box>
+          </VStack>
         </Wrap>
       </VStack>
     </Center>
