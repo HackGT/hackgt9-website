1ccd300b364a3d3ddd4b6e3e32d33c627fce8ac4
Yu Pan <70486009+yu-pan-cpu@users.noreply.github.com>
2022-07-10 01:19:51 -0400

    add assets, position emerging assets

--- src/components/tracks_and_challenges/emerging_tracks/EmergingTracks.tsx
+++ src/components/tracks_and_challenges/emerging_tracks/EmergingTracks.tsx
@@ -1,4 +1,4 @@
-import { Center, Text, VStack } from "@chakra-ui/react";
+import { Center, Text, VStack, Image } from "@chakra-ui/react";
 import TD from "../TD";
 import styles from "../TracksChallenges.module.scss";
 
@@ -6,7 +6,22 @@ import et from "./emerging_tracks.json";
 
 const EmergingTracks = () => {
   return (
-    <div id={styles.general_track_container}>
+    <div id={styles.emerging_track_container}>
+      <Image
+        alt="pink circle thingie"
+        src="/svg/emerging_track/giant_pink_ellipse.svg"
+        id={styles.giant_pink_ellipse}
+      />
+      <Image
+        alt="blue thingie"
+        src="/svg/emerging_track/faint_blue_ellipse.svg"
+        id={styles.faint_blue_ellipse}
+      />
+      <Image
+        alt="yellow thing"
+        src="/svg/emerging_track/yellow_streak.svg"
+        id={styles.yellow_streak}
+      />
       <Center>
         <VStack>
           <Text className={styles.subheader}>Emerging Tracks</Text>
