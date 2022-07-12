999aed3dea1326398338c28cf6ad0468bc9b264f
Yu Pan <70486009+yu-pan-cpu@users.noreply.github.com>
2022-07-11 20:04:48 -0400

    add up down arrow to faq

--- src/components/faq/Question.tsx
+++ src/components/faq/Question.tsx
@@ -1,16 +1,32 @@
-import { Text } from "@chakra-ui/react";
+import { HStack, Text, Image } from "@chakra-ui/react";
+import { useState } from "react";
 import styles from "./Faq.module.scss";
 
 interface QuestionProps {
   question: string;
-  onClick: any;
+  open: boolean;
 }
 
 const Question = (qp: QuestionProps) => {
+  const [arrowDirection, setArrowDirection] = useState(false);
+
+  const showAnswer = () => {
+    setArrowDirection(!arrowDirection);
+  };
+
   return (
-    <Text onClick={qp.onClick} className={`${styles.question} ${styles.both}`}>
-      {qp.question}
-    </Text>
+    <HStack onClick={showAnswer}>
+      <Text className={`${styles.question} ${styles.both}`}>{qp.question}</Text>
+      {arrowDirection ? (
+        <Image alt="arrow down" src="/icon/arrows/akar-icons_chevron-down.png" />
+      ) : (
+        <Image
+          className={styles.flip}
+          alt="arrow up"
+          src="/icon/arrows/akar-icons_chevron-down.png"
+        />
+      )}
+    </HStack>
   );
 };
 
