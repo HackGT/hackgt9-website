24a32961e7c572987528ff6d4c8f6579c0f432ab
naomesk <68748456+naomesk@users.noreply.github.com>
2022-07-20 18:26:31 +0300

    make faq console a png

--- src/components/faq/Faq.tsx
+++ src/components/faq/Faq.tsx
@@ -18,7 +18,7 @@ const Faq = () => {
             objectFit="contain"
             id={styles.game_console}
             alt="game console"
-            src="/svg/faq/updated_game_console.svg"
+            src="/img/faq/game_console.png"
           />
         </VStack>
       </Center>
