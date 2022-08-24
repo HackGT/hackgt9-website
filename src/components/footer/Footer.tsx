0e3917f11e7c30c32ebb72c6d1c3d2ab71161cf6
Yu Pan <70486009+yu-pan-cpu@users.noreply.github.com>
2022-08-24 14:13:58 -0400

    add MLH code of conduct

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,23 +7,26 @@ const Footer = () => {
       <footer>
         <VStack className={styles.footer_content}>
           <HStack>
-            <Link href="https://www.instagram.com/thehexlabs/">
+            <Link href="https://www.instagram.com/thehexlabs/" target="_blank">
               <Image src="/icon/insta.svg" alt="instagram" />
             </Link>
-            <Link href="https://www.facebook.com/TheHexLabs">
+            <Link href="https://www.facebook.com/TheHexLabs" target="_blank">
               <Image src="/icon/fb.svg" alt="facebook" />
             </Link>
-            <Link href="https://twitter.com/TheHexLabs">
+            <Link href="https://twitter.com/TheHexLabs" target="_blank">
               <Image src="/icon/twitter.svg" alt="twitter" />
             </Link>
-            <Link href="https://github.com/HackGT">
+            <Link href="https://github.com/HackGT" target="_blank">
               <Image src="/icon/github.svg" alt="github" />
             </Link>
-            <Link href="https://hexlabs.org/">
+            <Link href="https://hexlabs.org/" target="_blank">
               <Image src="/icon/web.svg" alt="hexlabs" />
             </Link>
           </HStack>
           <Text>With 💜 from the HexLabs team</Text>
+          <Link href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" target="_blank">
+            MLH Code of Conduct
+          </Link>
         </VStack>
       </footer>
     </div>
